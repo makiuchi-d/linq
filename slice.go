2e58e54a0c21889814f0eb910eeadace87c13a15
makki_d <d.makiuchi@gmail.com>
2022-01-06 01:00:08 +0900

    From/ToSlice

---
+++ slice.go
@@ -0,0 +1,36 @@
+package linq
+
+type sliceEnumerator[T any] struct {
+	s []T
+	i int
+}
+
+// FromSlice generates an Enumerator[T] from a slice.
+func FromSlice[S ~[]T, T any](s S) Enumerator[T] {
+	return &sliceEnumerator[T]{s: []T(s)}
+}
+
+// ToSlice creates a slice from an Enumerator[T]
+func ToSlice[T any](src Enumerator[T]) ([]T, error) {
+	s := make([]T, 0)
+	for {
+		v, err := src.Next()
+		if err != nil {
+			if isEOC(err) {
+				break
+			}
+			return s, err
+		}
+		s = append(s, v)
+	}
+	return s, nil
+}
+
+func (e *sliceEnumerator[T]) Next() (v T, err error) {
+	if e.i >= len(e.s) {
+		return v, EOC
+	}
+	v = e.s[e.i]
+	e.i++
+	return v, nil
+}
