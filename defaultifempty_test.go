0b0a4ab7f0b173bef9f07fb2ec776e49a1184f3f
makki_d <d.makiuchi@gmail.com>
2022-04-29 23:38:25 +0900

    empty/defaultifempty

--- defaultifempty_test.go
+++ defaultifempty_test.go
@@ -4,7 +4,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/makiuchi-d/linq"
+	"github.com/makiuchi-d/linq/v2"
 )
 
 func TestDefaultIfEmpty(t *testing.T) {
--- empty.go
+++ empty.go
@@ -2,9 +2,11 @@ package linq
 
 type emptyEnumerator[T any] struct{}
 
-// Empty returns an empty IEnumerable<T> that has the specified type argument.
-func Empty[T any]() Enumerator[T] {
-	return emptyEnumerator[T]{}
+// Empty returns an empty IEnumerable[T] that has the specified type argument.
+func Empty[T any]() Enumerable[T] {
+	return func() Enumerator[T] {
+		return emptyEnumerator[T]{}
+	}
 }
 
 func (emptyEnumerator[T]) Next() (def T, _ error) {
