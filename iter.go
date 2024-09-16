ffbf93f295cffc27931e697047ecfd5e179daa1d
makki_d <d.makiuchi@gmail.com>
2024-09-16 13:09:04 +0900

    add FromIterator

--- iter.go
+++ iter.go
@@ -2,7 +2,10 @@
 
 package linq
 
-import "iter"
+import (
+	"context"
+	"iter"
+)
 
 // All returns an iterator function
 func (e Enumerable[T]) All() iter.Seq2[T, error] {
@@ -44,4 +47,28 @@ func (e OrderedEnumerable[T]) All() iter.Seq2[T, error] {
 			}
 		}
 	}
+}
+
+// FromIterator generates an IEnumerable[T] from an iterator function.
+func FromIterator[T any](ctx context.Context, it iter.Seq[T]) Enumerable[T] {
+	return Generator(ctx, func(yield func(T) error) error {
+		for v := range it {
+			if err := yield(v); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+}
+
+// FromIterator2 generates an IEnumerable[KeyValue[K,V]] from an iterator function.
+func FromIterator2[K comparable, V any](ctx context.Context, it iter.Seq2[K, V]) Enumerable[KeyValue[K, V]] {
+	return Generator(ctx, func(yield func(KeyValue[K, V]) error) error {
+		for k, v := range it {
+			if err := yield(KeyValue[K, V]{k, v}); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
 }
--- iter_test.go
+++ iter_test.go
@@ -3,6 +3,8 @@
 package linq_test
 
 import (
+	"context"
+	"reflect"
 	"testing"
 
 	"github.com/makiuchi-d/linq/v2"
@@ -21,5 +23,40 @@ func TestIterAll(t *testing.T) {
 	}
 	if c != 5 || s != 15 {
 		t.Fatalf("(c, s) = (%v, %v) wants (%v, %v)", c, s, 5, 15)
+	}
+}
+
+func TestFromIterator(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	e := linq.FromIterator(ctx, func(yield func(int) bool) {
+		_ = yield(1) && yield(2) && yield(3) && yield(4) && yield(5)
+	})
+	e = linq.Take(e, 3)
+
+	a, err := linq.ToSlice(e)
+	if err != nil {
+		t.Fatalf("%+v", err)
+	}
+	exp := []int{1, 2, 3}
+	if !reflect.DeepEqual(a, exp) {
+		t.Fatalf("ToSlice: %v, wants %v", a, exp)
+	}
+}
+
+func TestFromIterator2(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	e := linq.FromIterator2(ctx, func(yield func(int, string) bool) {
+		_ = yield(1, "one") && yield(2, "two") && yield(3, "three")
+	})
+
+	a, err := linq.ToMap(e)
+	if err != nil {
+		t.Fatalf("%+v", err)
+	}
+	exp := map[int]string{1: "one", 2: "two", 3: "three"}
+	if !reflect.DeepEqual(a, exp) {
+		t.Fatalf("ToMap: %v, wants %v", a, exp)
 	}
 }
